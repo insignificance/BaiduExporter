b18afaa6d16491cdc0c69747a88c61850b607b29
Stalker <sqc1999@outlook.com>
2017-01-17 12:20:15 +0800

    Format files.

--- chrome/webpack.config.js
+++ chrome/webpack.config.js
@@ -2,19 +2,19 @@ var webpack = require('webpack');
 var path = require('path');
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 module.exports = {
-    context: __dirname + '/src',
-    entry: {
-        home: './home.js',
-        share: './share.js',
-        album: './album.js',
-        baidu:'./baidu.js',
-        inject:'./inject.js'
-    },
-    output: {
-        path:  __dirname + '/js',
-        filename: '[name].js' // 为上面entry的key值
-    },
-    plugins: [
-        // new webpack.optimize.UglifyJsPlugin(),
-    ],
+	context: __dirname + '/src',
+	entry: {
+		home: './home.js',
+		share: './share.js',
+		album: './album.js',
+		baidu: './baidu.js',
+		inject: './inject.js'
+	},
+	output: {
+		path: __dirname + '/js',
+		filename: '[name].js' // 为上面entry的key值
+	},
+	plugins: [
+		// new webpack.optimize.UglifyJsPlugin(),
+	],
 };
